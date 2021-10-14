2e4335c6b75738bd65a9d6f9b4f63052ffeaced6
ignacy123 <ignacy.buczek@onet.pl>
2021-10-14 16:29:00 +0200

    Added with clauses to database interface implementation to help with readability

--- Server/database.py
+++ Server/database.py
@@ -22,86 +22,74 @@ def get_conn():
     return conn
     
 def all_activities():
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM activities")
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-    return rows
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM activities")
+            rows = cur.fetchall()
+            cur.close()
+            return rows
 
 def fav_activities(email):
     email_id = get_email_id(email)
-    
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT activities.id, name FROM fav_activities LEFT OUTER JOIN activities ON activities.id = fav_activities.activity_id WHERE email_id = %s;", (email_id, ))
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-    return rows
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT activities.id, name FROM fav_activities LEFT OUTER JOIN activities ON activities.id = fav_activities.activity_id WHERE email_id = %s;", (email_id, ))
+            rows = cur.fetchall()
+            return rows
     
 
 def get_activity_id(name):
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM activities WHERE name = %s", (name, ))
-    if cur.rowcount == 0:
-        cur.close()
-        conn.close()
-        raise NoSuchActivityException("Getting id of a non-existent activity")
-    id = cur.fetchone()[0]
-    return id
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM activities WHERE name = %s", (name, ))
+            if cur.rowcount == 0:
+                cur.close()
+                conn.close()
+                raise NoSuchActivityException("Getting id of a non-existent activity")
+            id = cur.fetchone()[0]
+            return id
 
 def get_email_id(email):
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM email WHERE email = %s", (email, ))
-    if cur.rowcount == 0:
-        cur.execute("INSERT INTO email (email) VALUES (%s)", (email, ))
-        conn.commit()
-        cur.execute("SELECT * FROM email WHERE email = %s", (email, ))
-    id = cur.fetchone()[0]
-    cur.close()
-    conn.close()
-    return id
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM email WHERE email = %s", (email, ))
+            if cur.rowcount == 0:
+                cur.execute("INSERT INTO email (email) VALUES (%s)", (email, ))
+                conn.commit()
+                cur.execute("SELECT * FROM email WHERE email = %s", (email, ))
+            id = cur.fetchone()[0]
+            return id
 
 def new_fav(email, name):
     activity_id = get_activity_id(name)
     email_id = get_email_id(email)
-    conn = get_conn()
-    cur = conn.cursor()
-    #when attempting to choose an activity that is already a favourite, just ignore
-    try:
-        cur.execute("INSERT INTO fav_activities (email_id, activity_id) VALUES (%s, %s)", (email_id, activity_id))
-    except psycopg2.errors.UniqueViolation as e:
-        pass
-    conn.commit()
-    cur.close()
-    conn.close()
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            #when attempting to choose an activity that is already a favourite, just ignore
+            try:
+                cur.execute("INSERT INTO fav_activities (email_id, activity_id) VALUES (%s, %s)", (email_id, activity_id))
+            except psycopg2.errors.UniqueViolation as e:
+                pass
+            conn.commit()
 
 def delete_fav(email, name):
     activity_id = get_activity_id(name)
     email_id = get_email_id(email)
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("DELETE FROM fav_activities WHERE email_id = %s AND activity_id = %s", (email_id, activity_id))
-    conn.commit()
-    cur.close()
-    conn.close()
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("DELETE FROM fav_activities WHERE email_id = %s AND activity_id = %s", (email_id, activity_id))
+            conn.commit()
 
 def add_event(email, start_date, end_date, name, recurrence):
     activity_id = get_activity_id(name)
     email_id = get_email_id(email)
-    conn = get_conn()
-    cur = conn.cursor()
-    if recurrence:
-        cur.execute("INSERT INTO recurrent_events (email_id, activity_id, start_time, end_time, type) VALUES (%s, %s, %s, %s, %s)", (email_id, activity_id, start_date, end_date, recurrence))
-    else:
-        cur.execute("INSERT INTO single_events (email_id, activity_id, start_time, end_time) VALUES (%s, %s, %s, %s)", (email_id, activity_id, start_date, end_date))
-    conn.commit()
-    cur.close()
-    conn.close()
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            if recurrence:
+                cur.execute("INSERT INTO recurrent_events (email_id, activity_id, start_time, end_time, type) VALUES (%s, %s, %s, %s, %s)", (email_id, activity_id, start_date, end_date, recurrence))
+            else:
+                cur.execute("INSERT INTO single_events (email_id, activity_id, start_time, end_time) VALUES (%s, %s, %s, %s)", (email_id, activity_id, start_date, end_date))
+            conn.commit()
     
 class NoSuchActivityException(Exception):
     """A non-existant activity is requested"""
