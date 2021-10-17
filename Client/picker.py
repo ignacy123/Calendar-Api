9449aba24c9214d2b94fb520a77f09a1a131b0ed
ignacy123 <ignacy.buczek@onet.pl>
2021-10-17 15:55:50 +0200

    Client now deals with invalid tokens.

---
+++ Client/picker.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import time
+import dateutil.tz
+import dateutil.parser
+
+rec_dict = {'n' : None, 'd' : "DAILY", 'w' : "WEEKLY", 'm' : "MONTHLY", 'y' : "YEARLY"}
+
+    
+def pick_date(hours_minutes = True):
+    pattern = '%y/%m/%d %H:%M'
+    if not hours_minutes:
+        pattern = '%y/%m/%d'
+    while(True):
+        line = input()
+        try:
+            date = datetime.strptime(line, pattern)
+            date = date.replace(tzinfo = dateutil.tz.tzoffset(None, time.localtime().tm_gmtoff))
+            return date
+        except:
+            print("Sorry, but this isn't quite right. Try again.")
+            
+def pick_recurrence():
+    while True:
+        print("Recurrent?\n n - No \n d - Daily \n w - weekly \n m - monthly \n y - yearly")
+        line = input()
+        if line in ['n', 'd', 'w', 'm', 'y']:
+            rec = rec_dict[line]
+            return rec
+        print("Sorry, try again.") 
